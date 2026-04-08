1011805ae960954dc44c1b8730d10b107fe4f7df
Tan Yoong Chuen <tychuen88@gmail.com>
2026-04-08 17:40:17 +0800

    08/04/2026 removed CaregiverRepository.

--- src/main/java/com/gosmart/backoffice/service/CaregiverService.java
+++ src/main/java/com/gosmart/backoffice/service/CaregiverService.java
@@ -40,48 +40,6 @@ public class CaregiverService {
     }
 
     /**
-     * Find caregivers by name.
-     */
-    public List<CaregiverEntity> findByName(String name) {
-        return caregiverRepository.findByName(name);
-    }
-
-    /**
-     * Find a caregiver by email.
-     */
-    public Optional<CaregiverEntity> findByEmail(String email) {
-        return caregiverRepository.findByEmail(email);
-    }
-
-    /**
-     * Check if a caregiver email exists.
-     */
-    public boolean existsByEmail(String email) {
-        return caregiverRepository.existsByEmail(email);
-    }
-
-    /**
-     * Find a caregiver by mobile number.
-     */
-    public Optional<CaregiverEntity> findByMobileNumber(String mobileNumber) {
-        return caregiverRepository.findByMobileNumber(mobileNumber);
-    }
-
-    /**
-     * Check if a caregiver mobile number exists.
-     */
-    public boolean existsByMobileNumber(String mobileNumber) {
-        return caregiverRepository.existsByMobileNumber(mobileNumber);
-    }
-
-    /**
-     * Find a caregiver by system user ID.
-     */
-    public Optional<CaregiverEntity> findByUserId(String userId) {
-        return caregiverRepository.findByUserId(userId);
-    }
-
-    /**
      * Delete a caregiver by ID.
      */
     public void deleteById(Long id) {
